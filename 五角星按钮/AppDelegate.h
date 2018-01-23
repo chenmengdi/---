91f6aa3f1cc862546eb7597bb0d7d6fd349e7e9b
chenmengdi <1599126082@qq.com>
2018-01-23 14:17:41 +0800

    Initial Commit

---
+++ 五角星按钮/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  五角星按钮
+//
+//  Created by 陈孟迪 on 2018/1/23.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 五角星按钮/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  五角星按钮
+//
+//  Created by 陈孟迪 on 2018/1/23.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
